d0853750f07516ce0b497865a77e8f75d1bc1541
Samu <eamarilla@creminox.com>
2025-01-03 15:18:27 -0300

    Finalizacion Login - Paletizado - Encajonado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import "./globals.css";
 import TablaAlertas from "./tablaalertas/page";
+import Paletizado from "./paletizado/page";
+import Encajonado from "./encajonado/page"
+import Login from "./login/page"
+import RecuperarContra from "./login/recuperarcontraseña/page"
 
 export default function RootLayout() {
   return (
     <html lang="en">
       <body>
-        <TablaAlertas />
+        <RecuperarContra />
       </body>
     </html>
   );
---
+++ src/app/login/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import cremImg from './IMG/creminox-logo.png';
+import style from './Login.module.css';
+
+function Login() {
+    return (
+        <>
+            <body className={style.contenedor}>
+                <div className={style.formularioLogin}>
+                    <Image className={style.img} src={cremImg} alt="Creminox" />
+                    <div className={style.inputUsuario}>
+                        <p className={style.inputsTextos}>Usuario</p>
+                        <input
+                            className={style.inputs} 
+                            type="text"
+                        />
+                    </div>
+                    <div className={style.inputContraseña}>
+                        <p className={style.inputsTextos}>Contraseña</p>
+                        <input 
+                            className={style.inputs}
+                            type="password" 
+                        />
+                    </div>
+                    <button className={style.botonIngresar}>Ingresar</button>
+                    <ul className={style.botonRecuperar}>
+                        <li><a href="/recuperarcontraseña">Recuperar contraseña</a></li>
+                    </ul>
+                </div>
+            </body>
+        </>
+    );
+};
+
+export default Login;
