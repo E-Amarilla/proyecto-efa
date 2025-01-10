ce638f162ed8f74548158dc0ace242692b6e7ac4
joacosedran <jledesma@creminox.com>
2025-01-10 08:36:37 -0300

    title bold

--- src/app/desmoldeo/equipox/EquipoXImg/EquipoXImg.jsx
+++ src/app/desmoldeo/equipox/EquipoXImg/EquipoXImg.jsx
@@ -29,7 +29,7 @@ const EquipoXImg = () => {
 
     return (
         <div className={style.all}>
-            <h1 className={style.title}>Equipo X</h1>
+            <h1 className={style.title}>EQUIPO X</h1>
             <div className={style.carousel}>
                 <button onClick={prevImage} className={style.button}>❮</button>
                 <Image 
