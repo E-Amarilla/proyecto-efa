13bd21b8d274b65571d26b35047b681090867da6
Samu <eamarilla@creminox.com>
2025-01-22 08:29:18 -0300

    COMIENZO camaras

---
+++ src/app/camaras/camara1/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import style from './camara1.module.css';
+//Imagenes
+import Image from "next/image";
+import maqImg from '@/assets/img/creminox.png';
+
+const Configuraciones = () => {
+    return (
+        <>
+            <div className={style.contenedor}>
+                <div className={style.contenedorImagen}>
+                    <Image className={style.img} src={maqImg} alt="Creminox" />
+                </div>
+                <h2 className={style.h2}>CAMARAS</h2>
+                <div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Configuraciones;
--- src/app/camaras/page.jsx
+++ src/app/camaras/page.jsx
@@ -5,19 +5,38 @@ import style from './camaras.module.css';
 import Image from "next/image";
 import maqImg from '@/assets/img/creminox.png';
 
-const Configuraciones = () => {
+const Camaras = () => {
     return (
         <>
-            <div className={style.contenedor}>
-                <div className={style.contenedorImagen}>
-                    <Image className={style.img} src={maqImg} alt="Creminox" />
+        <h1 className={style.h1}>CAMARAS</h1>
+            <div className={style.contenedores}>
+                <div className={style.contenedor}>
+                    <div className={style.contenedorImagen}>
+                        </div>
+                            <Image className={style.img} src={maqImg} alt="Creminox" />
+                            <h2 className={style.h2}>CAMARAS</h2>
+                        <div>
+                    </div>
                 </div>
-                <h2 className={style.h2}>CAMARAS</h2>
-                <div>
+                <div className={style.contenedor}>
+                    <div className={style.contenedorImagen}>
+                        </div>
+                            <Image className={style.img} src={maqImg} alt="Creminox" />
+                            <h2 className={style.h2}>CAMARAS</h2>
+                        <div>
+                    </div>
+                </div>
+                <div className={style.contenedor}>
+                    <div className={style.contenedorImagen}>
+                        </div>
+                            <Image className={style.img} src={maqImg} alt="Creminox" />
+                            <h2 className={style.h2}>CAMARAS</h2>
+                        <div>
+                    </div>
                 </div>
             </div>
         </>
     );
 };
 
-export default Configuraciones;
+export default Camaras;
