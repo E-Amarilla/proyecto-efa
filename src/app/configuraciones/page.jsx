9a1c790f4fe3bb68a6d3e395e0b365e6e7ca8700
Samu <eamarilla@creminox.com>
2025-02-05 17:10:05 -0300

    CONFIGURACIONES - ACTUALIZADO

--- src/app/configuraciones/page.jsx
+++ src/app/configuraciones/page.jsx
@@ -6,12 +6,16 @@ import style from './configuraciones.module.css';
 import Image from "next/image";
 import receta2 from '@/assets/img/RECETA2.png';
 
-import { useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
+import Link from "next/link";
 import AuthContext from "../context/AuthContext"
 
 import maqImg from '@/assets/img/creminox.png';
 import Selector from './selector/selector';
 import textstyle from './texto.module.css';
+
+import BotonAplicar from '../components/botones/aplicarcorrecion/botonaplicar';
+import SelectConfiguracion from '../components/botones/selectConfiguracion/selectConfiguracion'
 
 const Configuraciones = () => {
     const { data } = useContext(AuthContext); // Obtiene datos del contexto
@@ -43,72 +47,110 @@ const Configuraciones = () => {
         { id: 7, texto: 'DATO 15', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null', icono:receta2  },
         { id: 8, texto: 'DATO 16', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null', icono:receta2  },
         { id: 9, texto: 'DATO 17', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null', icono:receta2  },
-    ];  
+    ];
+
+    const datosCorrecionesTorre = [
+        { id: 1, texto: 'Coreccion_hBastidor', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'},
+        { id: 2, texto: 'Coreccion_hAjuste', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'},
+        { id: 3, texto: 'Coreccion_hAjusteN1', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'},
+        { id: 4, texto: 'Coreccion_DisteNivel', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'},
+        { id: 5, texto: 'Coreccion_hNivel1', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'},
+        { id: 6, texto: 'hDeltaeNiveles', dato: idRecetaActual !== undefined && idRecetaActual !== null ? idRecetaActual : 'null'}
+    ];
+    
+    const opcionesCorrecciones = [
+        { id: 1, nombre: 'TORRES' },
+        { id: 2, nombre: 'NIVELES' },
+    ];
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleOptionClick = (id) => {
+        setSelectedOption(prev => (prev === id ? null : id));
+    };
 
     return (
-        <>
-            <div className={style.all}>
-                <div className={style.Izq}>
-                    <div className={style.selector}>
-                        <Selector />
-                    </div>
-                    <div className={style.contenedor}>
-                        <div className={style.datosGen}>
-                            <ul className={style.lista}>
-                                {datosGeneralesIzq.map(({ id, texto, dato, icono }) => (
-                                    <li key={id} className={style.datoList}>
-                                        <div className={style.detallesDatos} href='EquipoX'>
-                                            <div className={style.texto}>
-                                                <h3 className={textstyle.subtitulo}>{texto}</h3>
-                                                <h4 className={textstyle.h4}>{dato}</h4>
-                                            </div>
-                                            <Image 
-                                                src={icono} 
-                                                alt={`Estado: ${id}`} 
-                                                className={style.icon} 
-                                            />
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
+        <div className={style.all}>
+            <div className={style.Izq}>
+                <div className={style.selector}>
+                    <SelectConfiguracion />
                 </div>
-
-                <div className={style.Med}>
-                    <div className={style.contenedor}>
+                <div className={style.contenedor}>
                     <div className={style.datosGen}>
-                            <ul className={style.lista}>
-                                {datosGeneralesDer.map(({ id, texto, dato, icono }) => (
-                                    <li key={id} className={style.datoList}>
-                                        <div className={style.detallesDatos} href='EquipoX'>
-                                            <div className={style.texto}>
-                                                <h3 className={textstyle.subtitulo}>{texto}</h3>
-                                                <h4 className={textstyle.h4}>{dato}</h4>
-                                            </div>
-                                            <Image 
-                                                src={icono} 
-                                                alt={`Estado: ${id}`} 
-                                                className={style.icon} 
-                                            />
+                        <ul className={style.lista}>
+                            {datosGeneralesIzq.map(({ id, texto, dato, icono }) => (
+                                <li key={id} className={style.datoList}>
+                                    <div className={style.detallesDatos}>
+                                        <div className={style.texto}>
+                                            <h3 className={textstyle.subtitulo}>{texto}</h3>
+                                            <h4 className={textstyle.h4}>{dato}</h4>
                                         </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={style.Der}>
-                    <div className={style.contenedor}>
-                        <div className={style.contenedorImagen}>
-                            <Image className={style.img} src={maqImg} alt="Creminox" />
-                        </div>
-                        <h2 className={style.h2}>CONFIGURACIONES</h2>
+                                        <Image src={icono} alt={`Estado: ${id}`} className={style.icon} />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
-        </>
+
+            <div className={style.Med}>
+                <div className={style.contenedor}>
+                    <div className={style.datosGen}>
+                        <ul className={style.lista}>
+                            {datosGeneralesDer.map(({ id, texto, dato, icono }) => (
+                                <li key={id} className={style.datoList}>
+                                    <div className={style.detallesDatos}>
+                                        <div className={style.texto}>
+                                            <h3 className={textstyle.subtitulo}>{texto}</h3>
+                                            <h4 className={textstyle.h4}>{dato}</h4>
+                                        </div>
+                                        <Image src={icono} alt={`Estado: ${id}`} className={style.icon} />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.Der}>
+                <div className={style.tituloCorreciones}>
+                    <h2 className={style.h2}>CORRECIONES</h2>
+                </div>
+                <div className={style.botonesCorreciones}>
+                    <ul className={style.navList}>
+                        {opcionesCorrecciones.map(({ id, nombre }) => (
+                            <li key={id} className={style.navItem}>
+                                <button
+                                    className={`${style.navLink} ${selectedOption === id ? style.active : ''}`}
+                                    onClick={() => handleOptionClick(id)}
+                                >
+                                    {nombre}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={style.contenedor}>
+                    <div className={style.datosGenCorreciones}>
+                        <ul className={style.lista}>
+                            {datosCorrecionesTorre.map(({ id, texto, dato }) => (
+                                <li key={id} className={style.datoListCorreciones}>
+                                    <div className={style.detallesDatos}>
+                                        <div className={style.texto}>
+                                            <h3 className={textstyle.subtitulo}>{texto}</h3>
+                                            <h4 className={textstyle.h4}>{dato} - <input className={style.inputCorreciones}></input> </h4>
+                                        </div>
+                                        <BotonAplicar className={style.botonAplicar} />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
