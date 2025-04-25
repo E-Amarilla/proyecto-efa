1cdf9041b75edb490f190ca8fd6b5376f9acdd50
joacosedran <jledesma@creminox.com>
2025-04-25 12:36:07 -0300

    Login estilos

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,18 +1,18 @@
 "use client";
 import { useState } from "react";
-import style from "./Login.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
-// Imagenes
 import Image from "next/image";
 import crem from "@/assets/img/creminox.png";
+import { useTranslation } from 'react-i18next';
 
 const Login = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const { t } = useTranslation('trad');
 
-  const handleLogin = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setLoading(true);
     try {
       sessionStorage.setItem('acceso', 'true');
@@ -23,48 +23,48 @@ const Login = () => {
   };
 
   return (
-    <div className={style.all}>
-      <div className={style.contenedor}>
+    <div className="flex min-h-[95vh] w-full items-center justify-center">
+      <div className="w-auto h-[55vh] gap-[15px] flex flex-col items-center justify-between p-[3rem_4rem_2rem_4rem] max-w-[1920px] text-[#D9D9D9] bg-[#131313] rounded-[15px]">
         <Image
           src={crem}
-          alt='Creminox'
-          className={style.imagen}
+          alt="Creminox"
+          className="flex w-[65%] p-[0px] h-auto"
         />
-        <div className={style.formularioLogin}>
-        <div className={style.inlab}>
-            <label htmlFor="username" className={style.inputsTextos}>
-              Username
+        
+        <form onSubmit={handleSubmit} className="w-[100%] h-3/5 flex flex-col justify-evenly mt-[-100px]">
+          <div className="flex flex-col gap-[5px] h-1/3">
+            <label className="flex font-bold text-[17px] tracking-[0.5px]">
+              {t('min.usuario')}
             </label>
             <input
-              type="text" 
-              className={style.inputs}
+              type="text"
+              className="bg-[#1f1f1f] p-[4px] rounded-[10px] w-[100%] h-[60%] flex items-center justify-center border-none"
             />
           </div>
 
-          <div className={style.inlab}>
-            <label className={style.inputsTextos}>
-              Contraseña
+          <div className="flex flex-col gap-[5px] h-1/3">
+            <label className="flex font-bold text-[17px] tracking-[0.5px]">
+            {t('min.contra')}
             </label>
             <input
               type="password"
-              className={style.inputs}
+              className="bg-[#1f1f1f] p-[4px] rounded-[10px] w-[100%] h-[60%] flex items-center justify-center border-none"
             />
           </div>
+
           <button 
-            className={style.botonIngresar} 
-            onClick={handleLogin}
-            disabled={loading}
+            type="submit"
+            className="bg-[#e82a31] mt-[5px] p-[4px] rounded-[10px] w-[100%] h-1/5 flex items-center justify-center border-none text-[#D9D9D9] font-bold cursor-pointer disabled:bg-[#a82328] disabled:cursor-not-allowed"
           >
-            {loading ? (
-              <div className={style.spinner}></div>
-            ) : (
-              "Acceder"
-            )}
+            {t('min.acceder')}
           </button>
-        </div>
-        
-        <Link className={style.signup} href="/login/recuperacion">
-            ¿Olvidó su contraseña? Recupérela aquí
+        </form>
+
+        <Link 
+          href="/login/recuperacion"
+          className="w-[100%] mt-[-100px] flex text-center justify-center text-[#5d5d5d] h-auto text-[14px] font-bold tracking-[0.5px] cursor-pointer hover:text-[#e82a31] transition-all duration-200 ease-in-out"
+        >
+          {t('min.recuperar')}
         </Link>
       </div>
     </div>
